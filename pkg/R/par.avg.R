4b8b5f6fd699cbef0bae1e37ea854565facf0016
prezez <prezez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2009-04-20 19:44:00 +0000

    par.avg fixed to accept NA's in weight
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mumin@15 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/par.avg.R
+++ pkg/R/par.avg.R
@@ -1,16 +1,18 @@
 `par.avg` <-
 function(x, se, npar, weight, alpha = 0.05) {
 
-	wx <- weighted.mean(x, weight)
-	x.sqdiff <- (x - wx)^2
+	weight[is.na(weight)] <- 0 # not really necessary
 
+	wx <- weighted.mean(x, weight, na.rm = TRUE)
+	x.sqdiff <- (x - wx)^2
 	xvar <- se^2
-	avar <- weighted.mean(xvar + x.sqdiff, weight)^2
-	ase <- weighted.mean(sqrt(xvar + x.sqdiff), weight)
+
+	avar <- weighted.mean(xvar + x.sqdiff, weight, na.rm = TRUE)^2
+	ase <- weighted.mean(sqrt(xvar + x.sqdiff), weight, na.rm = TRUE)
 
 	z <- c((qt(1 - (alpha / 2), npar) / qnorm(1 - (alpha / 2)))^2)
 
-	use <- weighted.mean(sqrt((xvar * z) + x.sqdiff), weight)
+	use <- weighted.mean(sqrt((xvar * z) + x.sqdiff), weight, na.rm = TRUE)
 
 	ci <- qnorm(1 - (alpha / 2)) * use
 
