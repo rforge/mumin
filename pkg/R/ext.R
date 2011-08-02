78b5ad58afa8cebca66209778be7119a07ff947f
prezez <prezez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-08-02 08:18:47 +0000

    Added missing "ext.R"
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mumin@87 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

---
+++ pkg/R/ext.R
@@ -0,0 +1,87 @@
+# Extends: glmmML      #########################################################
+
+`tTable.glmmML` <- function(model, ...) {
+    coef <- model$coefficients
+    se <- model$coef.sd
+    ret <- cbind(coef, se, coef/se, signif(1 - pchisq((coef/se)^2,
+        1)))
+    dimnames(ret) <- list(names(coef), c("Estimate", "Std. Error",
+        "z", "Pr(>|z|)"))
+  return(ret)
+}
+
+`logLik.glmmML` <- function(object, ...) {
+  ll <- (-object$deviance)/2
+  n <- length(object$coefficients)
+  attr(ll, "df") <- n + object$cluster.null.df - object$df.residual
+  attr(ll, "nobs") <- n + object$cluster.null.df
+  class(ll) <- "logLik"
+  ll
+}
+
+`nobs.glmmML` <- function(object, ...) length(object$coefficients) +
+	object$cluster.null.df
+
+# This is merely to get rid of the annoying behaviour in summary.glmML.
+# it does not do anything except for printing the model output.
+`summary.glmmML` <- function(object, ...) identity(object)
+
+# Extends: survival    #########################################################
+
+# logLik for survival::coxph model
+# https://stat.ethz.ch/pipermail/r-help/2006-December/122118.html
+# originally by Charles C. Berry, mod. by KB: correction for the null model
+`logLik.coxph` <- function(object,...) {
+# Thx to Mathieu Basille:
+    y <-  object$loglik[length(object$loglik)]
+	#y <-  if(length(object$loglik) > 1)
+	#	-1 * (object$loglik[1] - object$loglik[2]) else object$loglik
+    class(y) <- "logLik"
+	#attr(y,"nall") <-
+	attr(y, "nobs") <- object$n
+    attr(y,'df') <- if(is.null(object$coef)) 0 else sum(!is.na(object$coef))
+    return(y)
+}
+
+`nobs.coxph` <- function(object, ...) object$n
+
+# Extends: lme4        #########################################################
+
+`nobs.mer` <- function(object, nall = TRUE, ...) {
+	N <- object@dims[["n"]]
+	p <- object@dims[["p"]]
+	if (nall) return (N)
+	REML <- object@dims[['REML']]
+	N - REML * p
+}
+
+# Extends: nnet/spdep        #########################################################
+
+`nobs.sarlm` <-
+`nobs.spautolm` <-
+`nobs.multinom` <-
+function(object, ...) NROW(fitted(object))
+
+
+# No longer needed
+# Extends: nlme
+# `nobs.gls` <- function(object, nall = TRUE, ...) {
+	# p <- object$dims$p
+	# N <- object$dims$N
+	# if (nall) return (N)
+	# REML <- object$method == "REML"
+	# N - REML * p
+	# # p - the number of coefficients in the linear model.
+# }
+
+# `nobs.lme` <- function(object, nall = TRUE, ...) {
+	# N <- object$dims$N
+	# if (nall) return (N)
+	# p <- object$dims$ncol[object$dims$Q + 1]
+	# REML <- object$method == "REML"
+	# N - REML * p
+	# #N - the number of observations in the data,
+	# #Q - the number of grouping levels
+	# #ncol - the number of columns in the model matrix for each level of grouping from innermost to outermost
+	# #  (last two values are equal to the number of fixed effects and one).
+# }
