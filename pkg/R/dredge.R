ea0fcc2ab78bee4114e4fa0cf418cdc3e6a06506
prezez <prezez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-28 20:15:54 +0000

    dredge: checking each generated model for the right 'nobs' r.squaredLR gives an error for unrecognized model class.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mumin@152 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/R/dredge.R
+++ pkg/R/dredge.R
@@ -5,7 +5,8 @@ function(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc",
 
 	gmEnv <- parent.frame()
 	gmCall <- .getCall(global.model)
-
+	gmNobs <- nobs(global.model)
+	
 	if (is.null(gmCall)) {
 		gmCall <- substitute(global.model)
 		if(!is.call(gmCall)) {
@@ -163,7 +164,7 @@ function(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc",
 		ret.nchunk <- 25L
 		ret.ncol <- n.vars + nvarying + 3L + nextra
 		ret <- matrix(NA_real_, ncol = ret.ncol, nrow = ret.nchunk)
-		retCoefTable <- vector(ret.nchunk, mode = "list")
+		coefTables <- vector(ret.nchunk, mode = "list")
 	} else {
 		ret.nchunk <- nmax
 	}
@@ -276,6 +277,11 @@ function(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc",
 				allCoef = TRUE)
 
 			ll <- logLik(fit1)
+			nobs1 <- nobs(fit1)
+			if(nobs1 != gmNobs) warning(gettextf(
+				"number of observations in model #%d (%d) differs from that in the global model (%d)", 
+				iComb, nobs1, gmNobs))
+			
 			row1 <- c(mcoef1[allTerms], extraResult1,
 				df = attr(ll, "df"), ll = ll, ic = IC(fit1)
 			)
@@ -286,7 +292,7 @@ function(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc",
 			ret.nrow <- nrow(ret)
 			if(k > ret.nrow) { # append if necesarry
 				nadd <- min(ret.nchunk, nmax - ret.nrow)
-				retCoefTable <- c(retCoefTable, vector(nadd, mode = "list"))
+				coefTables <- c(coefTables, vector(nadd, mode = "list"))
 				ret <- rbind(ret, matrix(NA, ncol = ret.ncol, nrow = nadd),
 					deparse.level = 0L)
 					calls <- c(calls, vector("list", nadd))
@@ -295,7 +301,7 @@ function(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc",
 
 			ord[k] <- iComb
 			ret[k, retColIdx] <- row1
-			retCoefTable[[k]] <- attr(mcoef1, "coefTable")
+			coefTables[[k]] <- attr(mcoef1, "coefTable")
 		} else { # if evaluate
 			k <- k + 1L # all OK, add model to table
 		}
@@ -311,7 +317,7 @@ function(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc",
 		ret <- ret[i, , drop = FALSE]
 		ord <- ord[i]
 		calls <- calls[i]
-		retCoefTable <- retCoefTable[i]
+		coefTables <- coefTables[i]
 	}
 
 	if(nvarying) {
@@ -342,7 +348,7 @@ function(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc",
 
 	o <- order(ret[, ICName], decreasing = FALSE)
 	ret <- ret[o, ]
-	retCoefTable <- retCoefTable[o]
+	coefTables <- coefTables[o]
 
 	ret$delta <- ret[, ICName] - min(ret[, ICName])
 	ret$weight <- exp(-ret$delta / 2) / sum(exp(-ret$delta / 2))
@@ -357,168 +363,9 @@ function(global.model, beta = FALSE, evaluate = TRUE, rank = "AICc",
 		rank.call = attr(IC, "call"),
 		beta = beta,
 		call = match.call(expand.dots = TRUE),
-		coefTables = retCoefTable,
-		nobs = nobs(global.model),
+		coefTables = coefTables,
+		nobs = gmNobs,
 		vCols = varying.names
 	)
 } ######
 
-`subset.model.selection` <-
-function(x, subset, select, recalc.weights = TRUE, ...) {
-	if (missing(select)) {
-		if(missing(subset)) return(x)
-		e <- .substHas(substitute(subset))
-		i <- eval(e, x, parent.frame())
-		return(`[.model.selection`(x, i, recalc.weights = recalc.weights, ...))
-	} else {
-		cl <- match.call(expand.dots = FALSE)
-	    cl <- cl[c(1L, match(names(formals("subset.data.frame")), names(cl), 0L))]
-	    cl[[1L]] <- as.name("subset.data.frame")
-		ret <- eval(cl, parent.frame())
-		if(recalc.weights && ("weight" %in% colnames(ret)))
-			ret[, 'weight'] <- ret[, 'weight'] / sum(ret[, 'weight'])
-	    return(ret)
-	}
-}
-
-`[.model.selection` <-
-function (x, i, j, recalc.weights = TRUE, ...) {
-	ret <- `[.data.frame`(x, i, j, ...)
-	if (missing(j)) {
-		s <- c("row.names", "calls", "coefTables", "random.terms", "order")
-		k <- match(dimnames(ret)[[1L]], dimnames(x)[[1L]])
-		attrib <- attributes(x)
-		attrib[s] <- lapply(attrib[s], `[`, k)
-		attributes(ret) <- attrib
-		if(recalc.weights)
-			ret$weight <- ret$weight / sum(ret$weight)
-			#ret[, 'weight'] <- ret[, 'weight'] / sum(ret[, 'weight'])
-
-		if(!is.null(warningList <- attr(ret, "warnings")))
-			attr(ret, "warnings") <- warningList[sapply(warningList, attr, "id") %in% rownames(ret)]
-	} else {
-		cls <- class(ret)
-		class(ret) <- cls[cls != "model.selection"] # numeric or data.frame
-	}
-	return(ret)
-}
-
-`print.model.selection` <-
-function(x, abbrev.names = TRUE, warnings = getOption("warn") != -1L, ...) {
-	orig.x <- x
-	if(!is.null(x$weight)) x$weight <- round(x$weight, 3L)
-	xterms <- attr(x, "terms")
-	if(is.null(xterms)) {
-		print.data.frame(x, ...)
-	} else {
-		if(abbrev.names) xterms <- abbreviateTerms(xterms, 3L)
-
-		colnames(x)[seq_along(xterms)] <-  xterms
-		globcl <- attr(x, "global.call")
-		if(!is.null(globcl)) {
-			cat("Global model call: ")
-			print(globcl)
-			cat("---\n")
-			random.terms <- attr(getAllTerms(attr(x, "global")), "random.terms")
-			if(!is.null(random.terms)) random.terms <- list(random.terms)
-		} else random.terms <- attr(x, "random.terms")
-
-		cat("Model selection table \n")
-		dig <- c("R^2" = 4L, df = 0L, logLik = 3L, AICc = 1L, AICc = 1L,
-			AIC = 1L, BIC = 1L, QAIC = 1L, QAICc = 1L, ICOMP = 1L, Cp = 1L,
-			delta = 2L,	weight = 3L)
-
-		j <- match(colnames(x), names(dig), nomatch = 0L)
-		i <- sapply(x, is.numeric) & (j == 0L)
-		x[, i] <- signif(x[, i], 4L)
-		for(i in names(dig)[j]) x[, i] <- round(x[, i], digits = dig[i])
-
-		vLegend <- character(0L)
-		if(abbrev.names) {
-			vCols <- attr(x, "vCols")
-			vlen <- nchar(vCols)
-			if(!is.null(vCols)) {
-				for(i in vCols) {
-					z <- x[, i]
-					lev <- levels(z)
-					lev <- lev[!(lev %in% c("", "NULL"))]
-					z <- factor(z, levels = lev)
-					n <- nchar(i)
-					for(k in seq.int(n, 1L)) {
-						shlev <- abbreviateTerms(lev, k, deflate = TRUE)
-						if(all(nchar(shlev) <= n)) break;
-					}
-					x[, i] <- factor(z, labels = shlev)
-					if(any(j <- shlev != lev)) vLegend <- c(vLegend, paste(i,
-						": ", paste(shlev[j], "=", sQuote(lev[j]),
-						collapse = ", "), sep = ""))
-				}
-			}
-		}
-
-		uqran <- unique(unlist(random.terms, use.names = FALSE))
-		abbran <- abbreviateTerms(gsub("1 | ", "", uqran, fixed = TRUE), 1L,
-			deflate = TRUE)
-		colran <- vapply(random.terms, function(s) paste(abbran[match(s, uqran)],
-			collapse = "+"), "")
-
-		if(addrandcol <- length(unique(colran)) > 1L) {
-			k <- which(colnames(x) == "df")[1L]
-			x <- cbind(x[, 1L:(k - 1L)], random = colran, x[, k:ncol(x)])
-		}
-
-		print.default(as.matrix(x)[, !sapply(x, function(.x) all(is.na(.x))),
-			drop = FALSE], na.print = "", quote = FALSE)
-
-		if(abbrev.names && length(vLegend))
-			cat("Abbreviations:", vLegend, sep = "\n")
-
-		if(!is.null(random.terms)) {
-			if(addrandcol) {
-				cat("Random terms: \n")
-				cat(paste(abbran, "=", sQuote(uqran)), sep = "\n")
-			} else {
-				cat("Random terms (all models): \n")
-				cat(paste(sQuote(uqran)), sep = ", ")
-				cat("\n")
-			}
-
-		}
-		if (warnings && !is.null(attr(x, "warnings"))) {
-			cat("\n"); print.warnings(attr(x, "warnings"))
-		}
-	}
-	invisible(orig.x)
-}
-
-`update.model.selection` <- function (object, global.model, ..., evaluate = TRUE) {
-    cl <- attr(object, "call")
-    if (is.null(cl)) stop("need an object with call component")
-    extras <- match.call(expand.dots = FALSE)$...
-
-	if(!missing(global.model))
-		extras <- c(list(global.model = substitute(global.model)), extras)
-
-    if (length(extras)) {
-        existing <- !is.na(match(names(extras), names(cl)))
-        for (a in names(extras)[existing]) cl[a] <- extras[a]
-        if (any(!existing)) {
-            cl <- c(as.list(cl), extras[!existing])
-            cl <- as.call(cl)
-        }
-    }
-    return(if (evaluate) eval(cl, parent.frame()) else cl)
-}
-
-`coef.model.selection` <- function (object, ...)
-	object[, attr(object, "terms")]
-
-`logLik.model.selection` <- function (object, ...) {
-	nobs <- attr(object, "nobs")
-	n <- nrow(object)
-	ret <- vector(n, mode = "list")
-	for(i in 1:n) ret[[i]] <-
-		structure(object[i, "logLik"], df = object[i, "df"], nobs = nobs,
-			class = "logLik")
-	ret
-}
--- pkg/R/r.squared.R
+++ pkg/R/r.squared.R
@@ -3,31 +3,27 @@
 	mClasses <- c("glmmML", "lm", "lme", "gls", "mer", "unmarkedFit", "coxph",
 		"coxme")
 	mClass <- mClasses[inherits(x, mClasses, which = TRUE) != 0L][1]
-	if(is.na(mClass)) {
-		warning("null model may be incorrect for this class")
-		mClass <- "default"
-	}
+	if(is.na(mClass)) mClass <- "default"
 	formulaArgName <- "formula"
 	Fun <- "glm"
+	call2arg <- function(x) formals(match.fun(x[[1L]]))
 	switch(mClass,
 		glmmML = {
 			REML <- FALSE
 			if(is.null(cl$family)) cl$family <- as.name("binomial")
 		}, gls = {
-			arg <- formals(match.fun(cl[[1]]))
-			REML <- match.arg(cl$method, eval(arg$method)) == "REML"
+			REML <- match.arg(cl$method, eval(call2arg(cl)$method)) == "REML"
 			formulaArgName <- "model"
 			cl$weights <- NULL
 		}, lme = {
-			arg <- formals(match.fun(cl[[1L]]))
-			REML <- match.arg(cl$method, eval(arg$method)) == "REML"
+			REML <- match.arg(cl$method, eval(call2arg(cl)$method)) == "REML"
 			formulaArgName <- "fixed"
 			cl$weights <- NULL
 		}, mer = {
 			arg <- formals(match.fun(cl[[1L]]))
-			REML <- if(is.null(cl$REML)) arg$REML else cl$REML
+			REML <- if(is.null(cl$REML)) call2arg(cl)$REML else cl$REML
 		}, unmarkedFit = {
-			nm <- names(cl)[-1]
+			nm <- names(cl)[-1L]
 			if("formula" %in% nm) {
 				cl$formula <- ~1~1
 			} else {
@@ -38,9 +34,12 @@
 			Fun <- NA
 		}, coxph = , coxme = {
 			Fun <- "coxph"
-			cl$formula <- update(as.formula(cl$formula), . ~ 1)
-		}, {
+			cl$formula <- update.formula(eval(cl$formula), . ~ 1)
+		}, lm = {
 			REML <- FALSE
+		}, {
+			stop("do not know (yet) how to construct a null model for class ", 
+				paste(sQuote(class(x))))
 		}
 	)
 
@@ -50,9 +49,8 @@
 			names(cl)[names(cl) == formulaArgName] <- "formula"
 		cl$formula <- update(as.formula(cl$formula), . ~ 1)
 		cl$method <- cl$start <- cl$offset <- contrasts <- NULL
-		#cl <- cl[c(TRUE, names(cl)[-1] %in% names(formals(match.fun(cl[[1L]]))))]
-		cl <- cl[c(TRUE, names(cl)[-1] %in% names(formals("glm")))]
 	}
+	cl <- cl[c(TRUE, names(cl)[-1L] %in% names(call2arg(cl)))]
 	if(evaluate) eval(cl, envir = envir) else cl
 }
 
