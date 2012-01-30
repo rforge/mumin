0ce632427806a0dcaef8e266b211ed812d0114a5
prezez <prezez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-30 15:53:59 +0000

    NAMESPACE: registering the missing 'getAllTerms.zeroinfl' method Manual: updated r.squaredLR command www: updated packages & added R stylesheed
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mumin@157 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- www/index.php
+++ www/index.php
@@ -12,11 +12,10 @@ echo '<?xml version="1.0" encoding="UTF-8"?>';
 	PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN"
 	"http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
 <html xmlns="http://www.w3.org/1999/xhtml" xml:lang="en" lang="en">
-
   <head>
 	<meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
 	<title><?php echo $group_name; ?></title>
-	<link href="<?php echo $themeroot; ?>styles/estilo1.css" rel="stylesheet" type="text/css" />
+	<link href="R.css" rel="stylesheet" type="text/css" />
   </head>
 
 <body>
