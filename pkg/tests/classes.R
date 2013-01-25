ccb591907ea6f983859bb64d70a95c049b46d848
prezez <prezez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2013-01-25 11:43:26 +0000

    Added 'predict.gls' with 'se.fit' argument Documentation changes
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mumin@198 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/classes.R
+++ pkg/tests/classes.R
@@ -20,7 +20,9 @@ dd <- dredge(fm1Dial.gls, m.max = 2, m.min = 1, fixed=~pressure, varying = varyi
 
 models <- get.models(dd, 1:4)
 
-subset(dd, correlation == "corAR1(0.771, form = ~1 | Subject)", recalc.delta = TRUE)
+predict(fm1Dial.gls, se.fit = TRUE, newdata = Dialyzer[1:5, ])
+
+subset(dd, correlation == "AR1_0.771", recalc.delta = TRUE)
 
 ma <- model.avg(models, revised = TRUE)
 ms <- model.sel(models)
@@ -29,7 +31,10 @@ print(ms, abbr = TRUE)
 summary(ma)
 predict(ma)[1:10]
 
-predict(ma, newdata = Dialyzer[1:5, ])
+# testing predict replacement:
+fm1 <- lme(rate ~ (pressure + I(pressure^2) + I(pressure^3)) * QB, ~ 1 | Subject, data = Dialyzer)
+MuMIn:::predict.lme(fm1, newdata = Dialyzer[1:5, ], level = 0, se.fit = TRUE)
+
 
 detach(package:nlme); rm(list=ls())
 
