bd496e52b92c32c13392330e3e9acbf568cdc631
prezez <prezez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-04-08 23:12:14 +0000

    tests/classes.R split into multiple files per model class.
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mumin@454 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/coxme.R
+++ pkg/tests/coxme.R
@@ -1,55 +1,50 @@
-if(length(find.package(c("survival", "coxme"), quiet = TRUE)) == 2) {
+if (MuMIn:::testStart("survival", "coxme")) {
 
-library(coxme)
-library(MuMIn)
-options(na.action = "na.fail")
+    lung$temp <- with(lung, scale(cbind(age, wt.loss, meal.cal)))
+    lung1 <- na.omit(lung)
 
+    rfit0 <- coxme(Surv(time, status) ~ ph.ecog * ph.karno + (age | 1) + (wt.loss | 1),
+        data = lung1)
 
-lung$temp <- with(lung, scale(cbind(age, wt.loss, meal.cal)))
-lung1 <- na.omit(lung)
+    getAllTerms(rfit0)
+        
+    stopifnot(is.numeric(coeffs(rfit0)))
 
-rfit0 <- coxme(Surv(time, status) ~ ph.ecog * ph.karno + (age | 1) + (wt.loss | 1),
-	data = lung1)
+    dd <- dredge(rfit0, eval = TRUE, trace = TRUE)
+    coeffs(dd)
+    model.sel(dd, rank = AIC)
+    summary(ma <- model.avg(dd))
 
-getAllTerms(rfit0)
-	
-stopifnot(is.numeric(coeffs(rfit0)))
+    library(survival)
 
-dd <- dredge(rfit0, eval = TRUE, trace = TRUE)
-coeffs(dd)
-model.sel(dd, rank = AIC)
-summary(ma <- model.avg(dd))
+    fm0 <- coxph(formula = Surv(time, status) ~ 1, data = lung1)
+    fm1 <- coxph(formula = Surv(time, status) ~ temp, data = lung1)
+    fme <- coxme(formula = Surv(time, status) ~ ph.ecog + (temp | 1), data = lung1)
+    fme1 <- coxme(formula = Surv(time, status) ~ ph.ecog + (age | 1) + (wt.loss | 1), data = lung1)
+    fme2 <- coxme(formula = Surv(time, status) ~ ph.ecog + (wt.loss | 1), data = lung1)
+    fm2 <- coxph(formula = Surv(time, status) ~ temp *  ph.ecog, data = lung1)
 
-library(survival)
+    avg <- model.avg(dredge(fm2), fit = T)
 
-fm0 <- coxph(formula = Surv(time, status) ~ 1, data = lung1)
-fm1 <- coxph(formula = Surv(time, status) ~ temp, data = lung1)
-fme <- coxme(formula = Surv(time, status) ~ ph.ecog + (temp | 1), data = lung1)
-fme1 <- coxme(formula = Surv(time, status) ~ ph.ecog + (age | 1) + (wt.loss | 1), data = lung1)
-fme2 <- coxme(formula = Surv(time, status) ~ ph.ecog + (wt.loss | 1), data = lung1)
-fm2 <- coxph(formula = Surv(time, status) ~ temp *  ph.ecog, data = lung1)
-
-avg <- model.avg(dredge(fm2), fit = T)
-
-#avgpred(avg, type = "response")
+    #avgpred(avg, type = "response")
 
 
-(ms <- model.sel(fm0, fm1, fme, fme1, fme2))
+    (ms <- model.sel(fm0, fm1, fme, fme1, fme2))
 
-summary(avg <- model.avg(ms))
+    summary(avg <- model.avg(ms))
 
-predict(fm1, se.fit = T)
+    predict(fm1, se.fit = T)
 
 
-if(length(find.package("nlme", quiet = TRUE)) == 1) {
-	#fit1 <- lme(effort ~ Type, data=ergoStool, random= ~1|Subject/ran1, method="ML")
+    if(length(find.package("nlme", quiet = TRUE)) == 1) {
+        #fit1 <- lme(effort ~ Type, data=ergoStool, random= ~1|Subject/ran1, method="ML")
 
-	if(exists("lmekin", mode = "function", envir = asNamespace("coxme"))) {
-		data("ergoStool", package = "nlme")
-		fit2 <- lmekin(effort ~ Type + (1|Subject), data = ergoStool)
-		dd <- dredge(fit2, trace = TRUE)
-		summary(ma <- model.avg(dd))
-	}
-}
+        if(exists("lmekin", mode = "function", envir = asNamespace("coxme"))) {
+            data("ergoStool", package = "nlme")
+            fit2 <- lmekin(effort ~ Type + (1|Subject), data = ergoStool)
+            dd <- dredge(fit2, trace = TRUE)
+            summary(ma <- model.avg(dd))
+        }
+    }
 
 }
