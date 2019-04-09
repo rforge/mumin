3b87cf0f95479c48a39a08a3c4f9f1825a9532ab
prezez <prezez@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2019-04-09 00:31:07 +0000

    tests/glm.R
    git-svn-id: svn://svn.r-forge.r-project.org/svnroot/mumin@456 edb9625f-4e0d-4859-8d74-9fd3b1da38cb

--- pkg/tests/glm.R
+++ pkg/tests/glm.R
@@ -1,4 +1,5 @@
 library("MuMIn")
+options(na.action = "na.fail")
 data(Orthodont, package = "nlme")
     
 fm1 <- lm(distance ~ Sex * age + age * Sex, data = Orthodont)
@@ -27,7 +28,7 @@ rm(list = ls())
 data(Cement, package = "MuMIn")
 
 nseq <- function(x, len = length(x)) seq(min(x, na.rm = TRUE), 
-    max(x, na.rm = T), length = len)
+    max(x, na.rm = TRUE), length = len)
 
 fm1 <- glm(y ~ (X1 + X2 + X3)^2, data = Cement)
 dd <- dredge(fm1)
@@ -38,7 +39,7 @@ summary(ma <- model.avg(gm))
 
 vcov(ma)
 
-summary(ma1 = model.avg(dd[1L:10L]))
+summary(ma1 <- model.avg(dd[1L:10L]))
 summary(ma2 <- model.avg(model.sel(dd[1L:10L], rank = "AICc")))
 all.equal(ma$avg.model, ma1$avg.mode)
 
